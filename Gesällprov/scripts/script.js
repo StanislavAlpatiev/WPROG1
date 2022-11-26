849d468a5122cd80f1c33f25102261a188e73d60
StanislavAlpatiev <alpatiev.stanislav@gmail.com>
2022-11-26 15:07:29 +0100

    added read more function to vegitation article

--- Gesällprov/scripts/script.js
+++ Gesällprov/scripts/script.js
@@ -5,3 +5,21 @@ $("#next").click(function(){
 $("#prev").click(function(){
 	$("#slider").prepend($("#slider img:last-of-type"));
 });
+
+$(document).ready(function(){
+	let maxLength = 300;
+	$(".show-read-more").each(function(){
+		let myStr = $(this).text();
+		if($.trim(myStr).length > maxLength){
+			let newStr = myStr.substring(0, maxLength);
+			let removedStr = myStr.substring(maxLength, $.trim(myStr).length);
+			$(this).empty().html(newStr);
+			$(this).append(' <a href="javascript:void(0);" class="read-more" style="color: white;">read more...</a>');
+			$(this).append('<span class="more-text">' + removedStr + '</span>');
+		}
+	});
+	$(".read-more").click(function(){
+		$(this).siblings(".more-text").contents().unwrap();
+		$(this).remove();
+	});
+});
