7db1ea18f1d2f6c0a90d847d091dc9d58622d540
StanislavAlpatiev <alpatiev.stanislav@gmail.com>
2022-11-29 01:27:07 +0100

    changed read more function

--- Gesällprov/scripts/script.js
+++ Gesällprov/scripts/script.js
@@ -9,41 +9,17 @@ $("#prev").click(function() {
 //This function creates a link inside text that says read more.
 //If link is clicked it expands the amount of text to be displayed.
 $(document).ready(function() {
-	//This is the max length of text to be displayed before read more is displayed.
-	let maxLength = $(".show-read-more").text().length / 3;
-	//Function targeting the paragraph that has read more functionality.
-	$(".show-read-more").each(function(){
-		//Variable storing the paragraph text.
-		let articleText = $(this).text();
-		//If amount of charachters excluding whitespaces is greater then maxLength, add read more... link.
-		if($.trim(articleText).length > maxLength) {
-			//Amount of text to show.
-			let newArticleTextToDisplay = articleText.substring(0, maxLength);
-			//Var storing text that does not fitt withing maxLength.
-			let removedStr = articleText.substring(maxLength, articleText.length);
-			//Resets the paragraph content with text that fits maxLength.
-			$(this).empty().html(newArticleTextToDisplay);
-			//Adds read more... link.
-			$(this).append('<a href="javascript:void(0);" class="read-more" style="color: white;"> ...read more</a>');
-			//Adds span containing text that did not fitt maxLength.
-			$(this).append('<span class="more-text" style="display: none;">' + removedStr + '</span>');
-			//Adds read less... link.
-			$(".more-text").append('<a href="javascript:void(0);" class="read-less" style="color: white;">read less</a>');
-		}
-	});
 	//Function that unwraps text when ...read more link is clicked. 
 	//Hide read more... link afterwards.
-	$(".read-more").click(function() {
-		$(this).hide();
-		$(".more-text").show();
+	$(".read-more-less").click(function() {
+		let buttonText = $(this).text();
+		$(".more-text").toggleClass('more-text-show');
+		if(buttonText == "Read more...") {
+			$(this).text("Read less.");
+		} else {
+			$(this).text("Read more...");
+		}
 	});
-	//Function that removes text when read less link is clicked. 
-	//Showes read more... linked afterwards.
-	$(".read-less").click(function() {
-		$(".more-text").hide();
-		$(".read-more").show();
-	});
-	
 });
 
 //Function that makes hamburger menu toggle content on click.
