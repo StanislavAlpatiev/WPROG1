908778640a0e268fdb0ac2d86b34d36f61aeda1b
StanislavAlpatiev <alpatiev.stanislav@gmail.com>
2022-12-03 02:48:11 +0100

    added custom error messages for feedback form

---
+++ Gesällprov/scripts/feedback.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+	
+	//These are settings for how the form is validated with jquery.validate.
+	$("#commentForm").validate({
+		rules : {
+			otherTopic : {
+				required: function(element) {
+					return $("#select-native-2 :selected").val() == "other";
+				},
+				minlength: 4,
+			},
+			
+			name: {
+				required: true,
+				minlength: 2,
+			},
+			
+			email: {
+				required: true,
+				email: true,
+			},
+			
+			url: {
+				url: true,
+			},
+			
+			comment: {
+				required: true,
+				minlength: 10,
+			},
+		},
+		
+		messages : {
+			url: {
+				url: "Please enter a valid url.",
+			}
+		},
+	}); 
+	
+	//This sets otherTopic to be hiden when page is loaded.
+	$("#otherTopic").hide();
+	
+	//Function that toggles the otherTopic input feald between hide and show when select element is clicked and other option is selected.
+	$("#select-native-2").click(function () {
+	
+		//save value of selected option.
+		let selectedTopic = $("#select-native-2 :selected").val();
+		
+		//Toggle is option with value other is selected.
+		if(selectedTopic == "other") {
+			$("#otherTopic").show();
+			console.log("other");
+		} else {
+			$("#otherTopic").hide();
+			$("#otherTopic").val("");
+		}
+	});
+});
+
